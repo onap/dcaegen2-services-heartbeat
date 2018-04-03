2dd94eeca32d501d6ddbcae4fa0753acd8db80a4
Lusheng Ji <lji@research.att.com>
2018-04-02 22:42:25 -0400

    Fix docker tag based on CIMAN-132
    Also specify the tox version.  If using the newest, 3.0.0,
    tox will fail due to not able to find pip.req.  So try the
    version of tox used by the last successful tox run, 2.9.1.
    
    Issue-ID: DCAEGEN2-206
    Change-Id: Iee6b635dc4fbe7e2b81d512627d7afd4097de635
    Signed-off-by: Lusheng Ji <lji@research.att.com>

--- mvn-phase-script.sh
+++ mvn-phase-script.sh
@@ -139,7 +139,8 @@ run_tox_test()
     virtualenv ./venv-tox
     source ./venv-tox/bin/activate
     pip install --upgrade pip
-    pip install --upgrade tox argparse
+    pip install --upgrade argparse
+    pip install tox==2.9.1
     pip freeze
     tox
     deactivate
@@ -237,7 +238,7 @@ build_and_push_docker()
   VERSION="${MVN_PROJECT_VERSION//[^0-9.]/}"
   VERSION2=$(echo "$VERSION" | cut -f1-2 -d'.')
 
-  LFQI="${IMAGENAME}:${VERSION}-${TIMESTAMP}"
+  LFQI="${IMAGENAME}:${VERSION}-${TIMESTAMP}"Z
   BUILD_PATH="${WORKSPACE}"
   # build a docker image
   docker build --rm -f "${WORKSPACE}"/Dockerfile -t "${LFQI}" "${BUILD_PATH}"
@@ -280,7 +281,7 @@ build_and_push_docker()
     fi
 
     OLDTAG="${LFQI}"
-    PUSHTAGS="${REPO}/${IMAGENAME}:${VERSION2}-${TIMESTAMP} ${REPO}/${IMAGENAME}:${VERSION2} ${REPO}/${IMAGENAME}:${VERSION2}-latest"
+    PUSHTAGS="${REPO}/${IMAGENAME}:${VERSION}-SNAPSHOT-${TIMESTAMP}Z ${REPO}/${IMAGENAME}:${VERSION} ${REPO}/${IMAGENAME}:latest"
     for NEWTAG in ${PUSHTAGS}
     do
       echo "tagging ${OLDTAG} to ${NEWTAG}"
