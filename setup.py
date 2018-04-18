042bc21ea4af4c8e5ee4cbbb0e3d0c89a70b7b8e
Lusheng Ji <lji@research.att.com>
2018-04-18 09:01:10 -0400

    Fix setup for pip 10.0.0
    Issue-ID: DCAEGEN2-452
    Change-Id: I22c2f3234408a588ebf664d53eae4737474cd800
    Signed-off-by: Lusheng Ji <lji@research.att.com>

--- setup.py
+++ setup.py
@@ -18,8 +18,16 @@
 
 import os
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
-from pip.download import PipSession
+#from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+#from pip.download import PipSession
+try: # for pip >= 10
+    from pip._internal.download import PipSession
+except ImportError: # for pip <= 9.0.3
+    from pip.download import PipSession
 
 setup(
     name='miss_htbt_service',
