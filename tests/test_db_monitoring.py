bbb0d5eb84d63ec736f3c4eb00257282c6e64f96
Vijay Venkatesh Kumar <vv770d@att.com>
2022-11-17 14:51:38 -0500

    Heartbeat code refactoring
    Firstpass - htbtworker and associated test refactored
    
    onap-gerrit-review: -changelog-or-release-file-missing
    
    Issue-ID: DCAEGEN2-2953
    Signed-off-by: Vijay Venkatesh Kumar <vv770d@att.com>
    Change-Id: I99229d966c13ad666ac994ab5a582aeeaa306639

---
+++ tests/test_db_monitoring.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# ============LICENSE_START=======================================================
+# Copyright (c) 2022 AT&T Intellectual Property. All rights reserved.
+# ================================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============LICENSE_END=========================================================
+
+import db_monitoring
+import htbtworker
+
+import requests
+import tempfile
+import os
+import json 
+import unittest
+
+from unittest.mock import *
+from _pytest.outcomes import skip
+
+# _logger = logging.getLogger(__name__)
+
+class Test_db_monitoring(unittest.TestCase):
+    def setUp(self):
+        htbtworker.configjsonfile = (os.path.dirname(__file__))+"/test-config.json"
+
+    @patch('requests.post')
+    def test_sendControlLoopEvent(self, mock1):
+        status = True
+        mock_resp = Mock()    
+        mock_resp.configure_mock(
+            **{
+                  "status_code": 200
+            }  
+         )
+        mock1.return_value = mock_resp  
+        db_monitoring.sendControlLoopEvent("ONSET", "ABC","1.0","vFW","vFW","VNF","NODE","1234567890","VFW","1.0","DCAE")
+        self.assertEqual(status, True)
+        db_monitoring.sendControlLoopEvent("ONSET", "ABC","1.0","vFW","vFW","VM","NODE","1234567890","VFW","1.0","DCAE")
+        self.assertEqual(status, True)
+        db_monitoring.sendControlLoopEvent("ABATED", "ABC","1.0","vFW","vFW","VNF","NODE","1234567890","VFW","1.0","DCAE")
+        self.assertEqual(status, True)
+        db_monitoring.sendControlLoopEvent("ABATED", "ABC","1.0","vFW","vFW","VM","NODE","1234567890","VFW","1.0","DCAE")
+        self.assertEqual(status, True)
+        
+if __name__ == "__main__": # pragma: no cover
+    unittest.main()
